7bf1ea0f8bf39c38b8abb5f64a366ddbc7fc3087
playboimau <137224099+Mauu1@users.noreply.github.com>
2024-10-31 14:26:38 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-        document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
-    });
-});
